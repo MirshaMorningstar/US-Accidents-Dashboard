2fd73a7f716faa34c7a641f435ea9478bc0da244
Mirsha Morningstar <84216040+MirshaMorningstar@users.noreply.github.com>
2025-07-06 12:41:04 +0530

    Update 1_Authentication🔐.py

--- 1_Authentication🔐.py
+++ 1_Authentication🔐.py
@@ -43,13 +43,39 @@ name, authentication_status, username = authenticator.login("Login your credenti
 
 if authentication_status is False:
     st.error("The provided Username or Password is incorrect ❌")
+    st.session_state.logged_in = False
 
 elif authentication_status is None:
     st.warning("Kindly enter your Username and Password 🔐")
 
 elif authentication_status:
-    st.session_state["logged_in"] = True
+    st.session_state.logged_in = True
     placeholder1.empty()
     placeholder2.empty()
     st.success(f"Welcome, {name} ✅")
+    
+    
+    # --- Show the sidebar menu after login ---
+    introduction = st.Page("pages/Introduction/2_About_This_Project.py", title="About this Project", icon=":material/search:")
+    
+    changetheme = st.Page("UI Personalization/3_Change_Theme.py", title="Change Application Theme", icon=":material/search:")
+    search = st.Page("UI Personalization/4_Color_Palette_Picker.py", title="Color Palette Picker", icon=":material/search:")
+    
+    search = st.Page("Data Pipeline/5_Data_Preprocessing_and_Preparation.py", title="Data Preprocessing and Preparation", icon=":material/search:")
+    search = st.Page("Data Pipeline/6_Visualisation_of_Data_Quality.py", title="Visualisation of Data Quality", icon=":material/search:")
+    search = st.Page("Data Pipeline/7_Feature_Engineering_and_Data_Correlation.py", title="Feature Engineering and Data Correlation Analysis", icon=":material/search:")
+    
+    search = st.Page("Exploratory Data Analysis/8_Specific_Exploration_of_Data.py", title="Search", icon=":material/search:")
+    search = st.Page("Exploratory Data Analysis/9_General_Exploration_of_Data.py", title="Search", icon=":material/search:")
+    search = st.Page("Exploratory Data Analysis/10_Inferential_Visualisations.py", title="Search", icon=":material/search:")
+    search = st.Page("Exploratory Data Analysis/search.py", title="Search", icon=":material/search:")
+    search = st.Page("tools/search.py", title="Search", icon=":material/search:")
+    search = st.Page("tools/search.py", title="Search", icon=":material/search:")
+    search = st.Page("tools/search.py", title="Search", icon=":material/search:")
+    search = st.Page("tools/search.py", title="Search", icon=":material/search:")
+    
+
+    
+        
+    
     st.switch_page("pages/UI Personalization/3_Change_Theme.py")  # Redirect after login
